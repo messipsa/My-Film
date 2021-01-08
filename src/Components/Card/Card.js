3c92ca78cf98d7207656533337ceed38c1249086
Messipsa <im_tamazouzt@esi.dz>
2021-01-08 03:10:58 +0100

    'Structure'

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+    render() {
+        return (
+            <div>
+                 <h2>Movie #1 :</h2>
+            </div>
+        );
+    }
+}
+
+export default Card;
---
+++ src/containers/List.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Card from '../Components/Card/Card';
+class List extends Component {
+    render() {
+        return (
+           
+            <Card />
+        );
+       
+    }
+}
+
+export default List;
